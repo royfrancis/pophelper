6a82a2b43aed36c94d46cb385dafd78a2dcb6740
Roy Francis <roymathewfrancis@gmail.com>
2020-02-11 23:23:53 +0100

    Fix old files

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-
-globalVariables(c("grp","k","elpdmean","elpdmax","elpdmin","lnk1","lnk1max","lnk1min","lnk2","lnk2max","lnk2min","deltaK","ind","run","order_ind","value","variable","labxpos","labypos","markerxpos","markerypos","divxpos","s2","grplab","order_cumulative"))
-
-.onAttach <- function(...){
-  packageStartupMessage("pophelper v2.3.1 ready.")
-}
-
